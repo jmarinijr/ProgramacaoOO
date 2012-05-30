5f086a859146500250dc94c098538187ce778f3a
unknown <Gordo@Gordobook.(none)>
2012-05-29 23:23:18 -0300

    Primeira classe

---
+++ src/br/edu/uniritter/exemplos/CifraCesar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.uniritter.exemplos;
+
+/**
+ *
+ * @author Gordo
+ */
+public class CifraCesar {
+    
+    private boolean testaLetra(int ascii){
+        if((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122) )
+            return true;
+        else
+            return false;
+    }
+
+    public String encripta(String texto, int deslocamento){
+        StringBuffer codificado = new StringBuffer();
+        int asciiOriginal, asciiCriptografado;
+        for(int i=0; i<texto.length(); i++){
+            asciiOriginal = (int) texto.charAt(i);
+            if(testaLetra(asciiOriginal)) {
+                asciiCriptografado = asciiOriginal + deslocamento;
+                if(testaLetra(asciiCriptografado))
+                    codificado.append((char) asciiCriptografado);
+                else
+                    codificado.append((char) (asciiCriptografado - 26));
+            }
+            else
+                codificado.append(texto.charAt(i));
+        }
+        String retorno = new String(codificado);
+        System.out.println(retorno);
+        return retorno;
+    }
+}
